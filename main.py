1c8db6b9a9f08578338d4e7d3fda68b854307c0c
enriqueMota <abelmota@live.com>
2023-05-17 11:06:27 -0400

    hangman game complete

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# Step 1
-
-import os
 import random
+from hangman_words import word_list
+from hangman_art import stages, logo
+import os
 
-
-word_list = ["aardvark", "baboon", "camel"]
 display = []
+
+# TODO-8
+lives = 6
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
@@ -32,22 +33,70 @@ display = []
 # The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_").
 # Then you can tell the user they've won.
 
+# TODO-8: - Create a variable called 'lives' to keep track of the number of lives left.
+# Set 'lives' to equal 6.
+
+# TODO-9: - If guess is not a letter in the chosen_word,
+# Then reduce 'lives' by 1.
+# If lives goes down to 0 then the game should stop and it should print "You lose."
+
+# TODO-10: - print the ASCII art from 'stages' that corresponds
+# to the current number of 'lives' the user has remaining.
+
 # TODO-1
 chosen_word = random.choice(word_list).lower()
+word_length = len(chosen_word)
 
-# TODO-2
-guess = input("Guess a letter from the word: ").lower()
 
-# TODO-3
-for char in chosen_word:
-    # TODO-4
-    if guess == char:
-        # TODO-5
-        display.append(char.upper())
-        # os.system("cls")
+# TODO-4
+for _ in range(word_length):
+    display.append("_")
+
+print(logo)
+print(f"the chosen word is: {chosen_word}")
+
+
+def guess_letter():
+    global lives
+
+    if not lives:
+        # TODO-10
+        print(stages[lives])
+        print("You lose.")
+        lives -= 1
+        return
+
+    # TODO-2
+    guess = input("Guess a letter from the word: ").lower()
+
+    if guess.upper() in display:
+        print("You already guessed this letter")
+        return
+
+    # TODO-5
+    for position in range(word_length):
+        char = chosen_word[position]
+
+        # TODO-3
+        if guess == char:
+            display[position] = char.upper()
+
+    if guess not in chosen_word:
+        # TODO-9
+        print("That's not in the word. You lose a life.")
+        print(stages[lives])
+        lives -= 1
+
+    if "_" not in display:
+        print(f"{' '.join(display)}")
+        print("You win")
     else:
-        display.append("_")
-        # print("Wrong")
+        # TODO-6
+        print(f"{' '.join(display)}")
+
+    return lives
 
 
-print(display)
+# TODO-7
+while "_" in display and lives >= 0:
+    guess_letter()
