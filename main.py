d41875e976b0d6142deebf230ce102f510afe908
enriqueMota <abelmota@live.com>
2023-05-17 00:00:15 -0400

    hangman step two

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
-#Step 1 
+# Step 1
 
+import os
 import random
 
 
 word_list = ["aardvark", "baboon", "camel"]
+display = []
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess.
+# Make guess lowercase.
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+# TODO-4 - Create an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"]
+# with 5 "_" representing each letter to guess
+
+# TODO-5 Loop through each position in the chosen_word;
+# If the letter at that position matches guess then
+# reveal that letter in the display at that position.
+# e.g If the user guessed "p" and the chosen word was
+# "apple", then display should be ["_", "P", "P", "_", "_"]
+
+# TODO-6: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+# TODO-7: - Use a while loop to let the user guess again.
+# The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_").
+# Then you can tell the user they've won.
 
 # TODO-1
 chosen_word = random.choice(word_list).lower()
+
 # TODO-2
 guess = input("Guess a letter from the word: ").lower()
 
 # TODO-3
 for char in chosen_word:
+    # TODO-4
     if guess == char:
-        print("Right")
+        # TODO-5
+        display.append(char.upper())
+        # os.system("cls")
     else:
-        print("Wrong")
+        display.append("_")
+        # print("Wrong")
+
+
+print(display)
